1c58d81c9c503c7bd2d1d91beb6956240db1fd87
Austin Louden <austinlouden@gmail.com>
2014-08-28 03:05:13 -0400

    simplify

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    $(".inner").hide(0).delay(500).fadeIn(1000)
+  
+    $("#top").nested({
+  		minWidth: 100,
+  		gutter: 5,
+  		animate: true,
+      animationOptions: {
+        speed: 20,
+        duration: 500,
+        queue: true
+      }
+	});
+
+	$("#bottom").nested({
+  		minWidth: 150,
+  		gutter: 10,
+  		animate: true,
+      animationOptions: {
+        speed: 20,
+        duration: 200,
+        queue: true
+      }
+	});
+
+  //$(".inner").hide(0).delay(500).fadeIn(1000);
+
+
 });
+
